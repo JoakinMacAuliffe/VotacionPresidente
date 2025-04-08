8b45e71acff79cf0eb910846e46d439d7b0a66ca
aasergiopinto <sergio.pinto1@mail.udp.cl>
2025-04-08 13:57:18 -0400

    Update Candidato.java

--- src/Candidato.java
+++ src/Candidato.java
@@ -1,1 +1,10 @@
-
+public class Candidato{
+	    
+	   private int id = 0;
+	   private String nombre, partido;
+	   private Queue <Integer> colaVotos = new LinkedList <>();
+	   
+	   public int añadeVoto(Voto v){
+	       
+	       colaVotos.push(v);
+	   }
